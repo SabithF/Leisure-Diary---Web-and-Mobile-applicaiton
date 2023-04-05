35bce4a403ccde7229decde57c515904842afc1a
Sabith Fariq <sabithfariq@gmail.com>
2023-04-05 18:05:35 +0530

    Accomodation and Form view

--- Leisure Land/Backend/server/routers/router.js
+++ Leisure Land/Backend/server/routers/router.js
@@ -13,5 +13,10 @@ route.get('/dashboard/accomodation', (req, res)=>{
     res.render('accomodation', {title: 'Accomodations- Leisure Diary'})
  });
 
+//  add new accomodation
+route.get('/create-accomodation', (req, res) =>{
+    res.render('create-accomodation', {title: 'Create New Accomodation'} )
+})
+
 
  module.exports = route;
